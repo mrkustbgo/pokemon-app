9682433e183ca721ca2dc4731dfc8a700ea63f5e
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-11-02 21:28:55 +0800

    features: add-css

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,7 +1,17 @@
+import { Box, Button, Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
+import Image from "next/image";
 
 const SearchBar: NextPage = () => {
-  return <div>SearchBar</div>;
+  return (
+    <Box className="">
+      <Button>
+        <Image src="/icon-home.svg" height={24} width={24} alt="Icon Home" />
+        Home
+      </Button>
+      <Flex></Flex>
+    </Box>
+  );
 };
 
 export default SearchBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { PokemonList, SearchBar } from "../components";
 
 const Home: NextPage = () => {
   return (
     <>
       <Stack className="w-4/5 m-auto">
-        
+        <SearchBar />
+        <PokemonList />
       </Stack>
     </>
   );
